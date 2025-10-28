5e538757c6e0991bedaecd144abee654ec7699f0
Zhen Peng <hi.pengzhen@gmail.com>
2025-10-27 21:23:08 -0700

    minor updates in bash

--- pg00_submit_job/sbatch/run00.run_exachem_remote.sh
+++ pg00_submit_job/sbatch/run00.run_exachem_remote.sh
@@ -1,3 +1,6 @@
 python ../scripts/py00.run_exachem.py \
     --input ../submodules/exachem/inputs/ozone.json \
     --account BR26_PENG599
+# python ../scripts/py00.run_exachem.py \
+#     --input ../submodules/exachem/inputs/lih.json \
+#     --account BR26_PENG599
